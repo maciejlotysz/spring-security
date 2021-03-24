919c5017a984f9b921d2fbe9c8fcf58499afd442
Maciej Lotysz <maciek.lotysz@gmail.com>
2021-03-24 20:13:30 +0100

    Create simple Controller and basic security config

---
+++ src/main/java/com/redbeardsoftware/springsecurity/student/Student.java
@@ -0,0 +1,15 @@
+package com.redbeardsoftware.springsecurity.student;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class Student {
+
+    private final Integer studentId;
+    private final String studentName;
+
+
+
+}
---
+++ src/main/java/com/redbeardsoftware/springsecurity/student/StudentController.java
@@ -0,0 +1,31 @@
+package com.redbeardsoftware.springsecurity.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "James Bond"),
+            new Student(2, "Killmonger"),
+            new Student(3, "Thor Odinsson"),
+            new Student(4, "Tony Stark"),
+            new Student(5, "Peter Parker")
+    );
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student " + studentId + " doesn't exist!"));
+    }
+
+}
