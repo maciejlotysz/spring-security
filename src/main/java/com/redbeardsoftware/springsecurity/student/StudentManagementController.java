c2eaf1ba247ce8672914428e155cbee58d502d99
Maciej Lotysz <maciek.lotysz@gmail.com>
2021-03-25 14:55:29 +0100

    Added Authorities by annotations

--- src/main/java/com/redbeardsoftware/springsecurity/student/StudentManagementController.java
+++ src/main/java/com/redbeardsoftware/springsecurity/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.redbeardsoftware.springsecurity.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -17,22 +18,28 @@ public class StudentManagementController {
             new Student(5, "Peter Parker")
     );
 
+//    hasRole('ROLE_')  hasANyRole('ROLE_')  hasAuthority('permission')  hasAnyAuthority('permission')
+
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN, ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerStudent(@RequestBody Student student) {
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
+     @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId,@RequestBody Student student) {
         System.out.printf("%s %s%n", studentId, student);
     }
